f8cd01105dea13812d9d3bf5e67bf6891436d5f0
shuabrannigan <joshua.wddg@gmail.com>
2023-01-24 12:37:24 +1030

    map visible on page - working

--- src/app/pages/root-page/root-page.component.ts
+++ src/app/pages/root-page/root-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-root-page',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RootPageComponent implements OnInit {
 
+  map: Map | undefined
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  mapLoaded($event: any) {
+    // $event as Map a little hacky, but works.
+    this.map = $event as Map
+    this.map.resize()
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, NgxMapboxGLModule],
   declarations: [],
-  exports: [CommonModule, FormsModule, MaterialModule],
+  exports: [CommonModule, FormsModule, MaterialModule, NgxMapboxGLModule],
 })
 export class SharedModule {}
