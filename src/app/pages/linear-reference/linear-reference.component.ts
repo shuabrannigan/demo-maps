e8f9bb96d3b3a1a85419c33e7510c2c36eba3052
shuabrannigan <joshua.wddg@gmail.com>
2023-01-28 10:07:11 +1030

    created componenet wired in routing

---
+++ src/app/pages/linear-reference/linear-reference.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linear-reference',
+  templateUrl: './linear-reference.component.html',
+  styleUrls: ['./linear-reference.component.scss']
+})
+export class LinearReferenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
