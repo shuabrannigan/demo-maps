4f94950b1acf4d7c72fb69088bb8f6713b15840a
shuabrannigan <joshua.wddg@gmail.com>
2023-02-08 22:16:51 +1030

    call maploaded and load check map

--- src/app/pages/featurecollection-view/featurecollection-view.spec.ts
+++ src/app/pages/featurecollection-view/featurecollection-view.spec.ts
@@ -1,7 +1,15 @@
 import { DebugElement } from "@angular/core"
-import { ComponentFixture, TestBed } from "@angular/core/testing"
-import { StoreModule } from "@ngrx/store"
-import { provideMockStore } from "@ngrx/store/testing"
+import {
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { provideMockStore } from '@ngrx/store/testing';
+import { Map } from 'mapbox-gl';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { environment } from 'src/environments/environment';
 import { FeaturecollectionViewComponent } from "./featurecollection-view.component"
 
 
@@ -11,24 +19,44 @@ describe('FeatureCollectionViewComponent', () => {
     let component: FeaturecollectionViewComponent
     let fixture: ComponentFixture<FeaturecollectionViewComponent>
     let de: DebugElement
+    let map: Map
 
     beforeEach(async () => {
-        TestBed.configureTestingModule({
-            imports: [StoreModule],
-            declarations: [FeaturecollectionViewComponent],
-            providers: [provideMockStore({})]
-        }).compileComponents()
-    })
+      TestBed.configureTestingModule({
+        imports: [
+          RouterTestingModule,
+          StoreModule,
+          SharedModule,
+          NgxMapboxGLModule.withConfig({
+            accessToken: environment.MAPBOX_API_KEY,
+          }),
+        ],
+        declarations: [FeaturecollectionViewComponent],
+        providers: [provideMockStore({})],
+      }).compileComponents();
+    });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(FeaturecollectionViewComponent)
-        component = fixture.componentInstance
-        de = fixture.debugElement
+      fixture = TestBed.createComponent(FeaturecollectionViewComponent);
+      component = fixture.componentInstance;
+      de = fixture.debugElement;
+      fixture.detectChanges();
+    });
 
-        fixture.detectChanges()
+    beforeEach(() => {
+      let el = document.createElement('div')
+      el.id = 'map'
+      map = new Map({container: el})
     })
 
-    it ('should create component', () => {
-        expect(component).toBeTruthy()
-    })
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('call mapLoaded(), load check map is loaded', async () => {
+      component.mapLoaded(map)
+      expect(component.map?.loaded).toBeTruthy()
+    });
+
 })
