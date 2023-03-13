03fb8ecf80ec3eb9f9844b29801f57c321a1c260
shuabrannigan <joshua.wddg@gmail.com>
2023-03-13 15:36:49 +1030

    added basic navigtion tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,20 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
+describe('Root Page', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('has feature viewer button and navigates', () => {
+    let button = cy.get('[data-test="feature-viewer"]')
+    button.should('exist')
+    button.click();
+    cy.location('pathname').should('match', /\/feature-viewer$/)
+  });
+
+  it('has linear reference button and navigates', () => {
+    let button = cy.get('[data-test="linear-reference"]')
+    button.should('exist')
+    
+    button.click()
+    cy.location('pathname').should('match', /\/linear-reference$/)
   })
-})
+});
