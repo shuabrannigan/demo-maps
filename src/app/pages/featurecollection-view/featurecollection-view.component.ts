4fe8e75ce2dcce166019a6535b6e61a74801fe71
shuabrannigan <joshua.wddg@gmail.com>
2023-03-31 20:53:39 +1030

    created service for featureviewmap

--- src/app/pages/featurecollection-view/featurecollection-view.component.ts
+++ src/app/pages/featurecollection-view/featurecollection-view.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
 import { FeatureCollectionViewService } from './featurecollection-view.service';
 import { Observable } from 'rxjs';
+import { FeatureCollectionViewMapService } from './featurecollection-view-map.service';
+import { AbstractBaseMapService } from 'src/app/shared/components/basemap/abstract-basemap';
 
 @Component({
   selector: 'app-featurecollection-view',
   templateUrl: './featurecollection-view.component.html',
   styleUrls: ['./featurecollection-view.component.scss'],
-  providers: [FeatureCollectionViewService],
+  providers: [FeatureCollectionViewService, FeatureCollectionViewMapService],
 })
 export class FeaturecollectionViewComponent {
-  constructor(public fcvs: FeatureCollectionViewService) {}
+  constructor(public fcvs: FeatureCollectionViewService, @Inject(FeatureCollectionViewMapService) public fcms: AbstractBaseMapService) {}
 
   formInput: Observable<string> =  this.fcvs.getFeatureCollectionAsJson$();
   error$: Observable<boolean> = this.fcvs.error$
